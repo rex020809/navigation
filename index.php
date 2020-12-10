f6273903d4581e94091798001147601c5f080822
rex020809 <rex020809.tem06@nctu.edu.tw>
2020-12-11 00:02:34 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $url1="";
+            $url2="";
+            $url3="";
+            $url=[$url1, $url2, $url3];
+            echo $url1
+        ?>
+    </body>
+</html>
